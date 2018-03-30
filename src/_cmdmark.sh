ce0281678e2ddc761508038fee751b7531753501
Angelo Carrabba <acarrabba6@gmail.com>
2018-03-29 21:46:53 -0400

    made completion more relevant at certain points

--- src/_cmdmark.sh
+++ src/_cmdmark.sh
@@ -3,25 +3,34 @@
 ###############################################################################
 function _cmdmark {
     local cur=${COMP_WORDS[COMP_CWORD]}
+    COMPREPLY=( )
+
 
     if (( $COMP_CWORD < 2 )); then
 	local commands=$(cat $savedCommands | sed 's/^(\([^)]*\)).*$/\1/' | grep -ve '^$')
 	COMPREPLY=( $(compgen -W "$commands -set -delete -list -help") )
-    elif [[ $COMP_CWORD  == "2" && "${COMP_WORDS[1]}" == "-delete" ]]; then
-	local commands=$(cat $savedCommands | sed 's/^(\([^)]*\)).*$/\1/' | grep -ve '^$')
-	COMPREPLY=( $(compgen -W "$commands") )
-    elif [[ "${COMP_WORDS[1]}" == "-list" || "${COMP_WORDS[1]}" == "-help" ]]; then
-	COMPREPLY=( )
-    elif [[ "${COMP_WORDS[1]}" == "-set" ]]; then
-	if (( $COMP_CWORD ==  2 )); then
-	    COMPREPLY=( $(compgen  -A "function" -abck) )
-	else
-	    COMPREPLY=( $(compgen -o "filenames" -A "file") )
-	fi
-
-    else
-	COMPREPLY=( $(compgen -o filenames -A file) )
+	return 0
     fi
+
+    case "${COMP_WORDS[1]}" in
+	'-set')
+	    case $COMP_CWORD in
+		2) COMPREPLY=( );;
+		3) COMPREPLY=( $(compgen  -A "function" -abck) );;
+		*) COMPREPLY=( $(compgen -o "filenames" -A "file") );;
+	    esac
+	    ;;
+	'-delete')
+	    case $COMP_CWORD in
+		2) COMPREPLY=( $(compgen -W "$(cat $savedCommands | sed 's/^(\([^)]*\)).*$/\1/' | grep -ve '^$')") );;
+		*) COMPREPLY=( );;
+	    esac
+	    ;;
+	'-help') COMPREPLY=( );;
+	'-list') COMPREPLY=( );;
+	*)
+	    COMPREPLY=( $(compgen -o "filenames" -A "file") );;
+    esac
     return 0
 }
 
