bed8abb5286838c8be80a60bdf483cc1199fcfdc
WSBF Computer Engineer <computer@wsbf.net>
2018-04-01 13:17:30 -0400

    realpath => readlink -m | realpath is not found

--- install.sh
+++ install.sh
@@ -2,6 +2,9 @@
 
 init=${1:-"$HOME/.bashrc"} # user can specify their bashrc location
 
+
+if [[ $(command -v realpath) ]]; then  expandPath="realpath"; else expandPath="readlink -m"; fi
+
 # get our directory name
 if [[ $1 = '' ]]; then
     if [[ "$(uname -s)" = 'Linux' ]]; then
@@ -9,12 +12,12 @@ if [[ $1 = '' ]]; then
     else
 	basedir=$(dirname "$(readlink "$0" || echo "$(echo "$0" | sed -e 's,\\,/,g')")")
     fi
-    basedir=$(dirname $(realpath $0))
+    basedir=$(dirname $($expandPath $0))
 
     filename=~/.savedCommands.dat
 else
 
-    filename=$(realpath $1)
+    filename=$($expandPath $1)
 fi
 
 
@@ -26,8 +29,8 @@ if [ ! -f $filename ]; then touch $filename; fi
 
 
 
-function src { if [[ ! $1 ]]; then echo $(realpath ./src); else echo $(realpath ./src)/$1; fi }
-function dst { if [[ ! $1 ]]; then echo $(realpath ./dst); else echo $(realpath ./dst)/$1; fi }
+function src { if [[ ! $1 ]]; then echo $($expandPath ./src); else echo $($expandPath ./src)/$1; fi }
+function dst { if [[ ! $1 ]]; then echo $($expandPath ./dst); else echo $($expandPath ./dst)/$1; fi }
 mkdir -p $(dst)
 
 cmdmark="cmdmark.sh"
